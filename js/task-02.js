fe4e55a8e17201764fdcebc259664c44de12418c
NatalieFalach <natkacandy@gmail.com>
2022-10-18 12:36:56 +0300

    Tesk 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const elementList = [];
+const ulElement = document.querySelector('#ingredients');
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  elementList.push(liEl);
+}
+
+ulElement.append(...elementList);
